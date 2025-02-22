16cf0d2e7f76158a881692a1393de4748165147b
Jin1 <jin1@iMac-von-TJ.local>
2025-02-22 11:26:57 +0100

    15.Mazesolver

---
+++ 15/MazeSolver/practice/main.c
@@ -0,0 +1,32 @@
+
+
+void	handle_error(int flg)
+{
+	if (flag == 0)
+	{
+
+	}
+	else if (flag == 1)
+	{
+		
+	}
+
+}
+
+void	get_next_step()
+void	move()
+
+void	open_map()
+void	validate_map()
+void	read_map()
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		printf("Map is not given\n");
+		return (0);
+	}
+
+
+}
